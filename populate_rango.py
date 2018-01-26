09763c8ad188feaad8b8e902350101e9b4a8393d
Ibai <2268745j@student.gla.ac.uk>
2018-01-26 16:40:08 +0000

    Finished half of chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -4,6 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 import django
 django.setup()
+
 from rango.models import Category, Page
 
 def populate():
@@ -30,13 +31,13 @@ def populate():
          {"title":"Flask",
           "url":"http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16} }
 
     #Iterating over dictionaries
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -51,8 +52,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name,views=views,likes=likes)[0]
     c.save()
     return c
 
